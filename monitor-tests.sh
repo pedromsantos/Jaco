0d3d234f54895363e108f5e403b6faa64ad77fb2
Pedro Santos <github@mariodelrio.com>
2016-03-06 00:20:15 +0000

    Change monitor-tests to use fsmonitor instead of fswatch

--- monitor-tests.sh
+++ monitor-tests.sh
@@ -4,4 +4,4 @@ XUNIT_PATH="./packages/xunit.runner.console.2.0.0/tools/xunit.console.x86.exe"
 TESTS_PATH="./**/bin/Debug/*Tests.dll"
 SOURCES_PATH="./**/*.cs"
 
-fswatch -0 $SOURCES_PATH | xargs -0 -n 1 -I {} mono $XUNIT_PATH $TESTS_PATH
+fsmonitor -s -p '+*.cs' mono $XUNIT_PATH $TESTS_PATH
