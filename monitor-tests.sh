3c5ef32446d4900de229690f50bb9a4b59ba6abb
Pedro Santos <github@mariodelrio.com>
2016-03-19 16:35:29 +0000

    monitor-tests.sh now invokes build.sh

--- monitor-tests.sh
+++ monitor-tests.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-XUNIT_PATH="./packages/xunit.runner.console.2.0.0/tools/xunit.console.x86.exe"  
-TESTS_PATH="./**/bin/Debug/*Tests.dll"
-SOURCES_PATH="./**/*.cs"
-
-fsmonitor -p '+*.cs' mono $XUNIT_PATH $TESTS_PATH
+fsmonitor -p '+*.cs' ./build.sh 
