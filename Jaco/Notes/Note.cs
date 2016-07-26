0d608a57c68e7b1a1845868c10f8b15d18bd12aa
Pedro Santos <pedro@codurance.com>
2016-07-26 11:41:00 +0100

    Remove lazy constructo from Note not needed

--- Jaco/Notes/Note.cs
+++ Jaco/Notes/Note.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,23 +5,23 @@ namespace Jaco.Notes
 {
     public class Note
     {
-        public static readonly Note C = new Lazy<Note>(() => new Note(Pitch.C, "C", Accident.None, MinNoteIndex)).Value;
-        public static readonly Note CSharp = new Lazy<Note>(() => new Note(Pitch.CSharp, "C#", Accident.Sharp, 1)).Value;
-        public static readonly Note DFlat = new Lazy<Note>(() => new Note(Pitch.DFlat, "Db", Accident.Flat, 2)).Value;
-        public static readonly Note D = new Lazy<Note>(() => new Note(Pitch.D, "D", Accident.None, 3)).Value;
-        public static readonly Note DSharp = new Lazy<Note>(() => new Note(Pitch.DSharp, "D#", Accident.Sharp, 4)).Value;
-        public static readonly Note EFlat = new Lazy<Note>(() => new Note(Pitch.EFlat, "Eb", Accident.Flat, 5)).Value;
-        public static readonly Note E = new Lazy<Note>(() => new Note(Pitch.E, "E", Accident.None, 6)).Value;
-        public static readonly Note F = new Lazy<Note>(() => new Note(Pitch.F, "F", Accident.None, 7)).Value;
-        public static readonly Note FSharp = new Lazy<Note>(() => new Note(Pitch.FSharp, "F#", Accident.Sharp, 8)).Value;
-        public static readonly Note GFlat = new Lazy<Note>(() => new Note(Pitch.GFlat, "Gb", Accident.Flat, 9)).Value;
-        public static readonly Note G = new Lazy<Note>(() => new Note(Pitch.G, "G", Accident.None, 10)).Value;
-        public static readonly Note GSharp =new Lazy<Note>(() => new Note(Pitch.GSharp, "G#", Accident.Sharp, 11)).Value;
-        public static readonly Note AFlat = new Lazy<Note>(() => new Note(Pitch.AFlat, "Ab", Accident.Flat, 12)).Value;
-        public static readonly Note A = new Lazy<Note>(() => new Note(Pitch.A, "A", Accident.None, 13)).Value;
-        public static readonly Note ASharp = new Lazy<Note>(() => new Note(Pitch.ASharp, "A#", Accident.Sharp, 14)).Value;
-        public static readonly Note BFlat = new Lazy<Note>(() => new Note(Pitch.BFlat, "Bb", Accident.Flat, 15)).Value;
-        public static readonly Note B = new Lazy<Note>(() => new Note(Pitch.B, "B", Accident.None, MaxNoteIndex)).Value;
+        public static readonly Note C = new Note(Pitch.C, "C", Accident.None, MinNoteIndex);
+        public static readonly Note CSharp = new Note(Pitch.CSharp, "C#", Accident.Sharp, 1);
+        public static readonly Note DFlat = new Note(Pitch.DFlat, "Db", Accident.Flat, 2);
+        public static readonly Note D =new Note(Pitch.D, "D", Accident.None, 3);
+        public static readonly Note DSharp = new Note(Pitch.DSharp, "D#", Accident.Sharp, 4);
+        public static readonly Note EFlat = new Note(Pitch.EFlat, "Eb", Accident.Flat, 5);
+        public static readonly Note E = new Note(Pitch.E, "E", Accident.None, 6);
+        public static readonly Note F = new Note(Pitch.F, "F", Accident.None, 7);
+        public static readonly Note FSharp = new Note(Pitch.FSharp, "F#", Accident.Sharp, 8);
+        public static readonly Note GFlat = new Note(Pitch.GFlat, "Gb", Accident.Flat, 9);
+        public static readonly Note G = new Note(Pitch.G, "G", Accident.None, 10);
+        public static readonly Note GSharp = new Note(Pitch.GSharp, "G#", Accident.Sharp, 11);
+        public static readonly Note AFlat = new Note(Pitch.AFlat, "Ab", Accident.Flat, 12);
+        public static readonly Note A = new Note(Pitch.A, "A", Accident.None, 13);
+        public static readonly Note ASharp = new Note(Pitch.ASharp, "A#", Accident.Sharp, 14);
+        public static readonly Note BFlat = new Note(Pitch.BFlat, "Bb", Accident.Flat, 15);
+        public static readonly Note B = new Note(Pitch.B, "B", Accident.None, MaxNoteIndex);
 
         public static IEnumerable<Note> Notes
         {
