ae8a29b45740e1655fa47791330a1b7dd0f0b932
Pedro Santos <pedromsantos@users.noreply.github.com>
2024-07-10 14:08:50 +0200

    chore: configure swagger

--- JacoAPI/Program.cs
+++ JacoAPI/Program.cs
@@ -1,5 +1,28 @@
+using NSwag.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+	config.DocumentName = "JacoAPI";
+	config.Title = "JacoAPI v1";
+	config.Version = "v1";
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+	app.UseOpenApi();
+	app.UseSwaggerUi(config =>
+	{
+		config.DocumentTitle = "JacoAPI";
+		config.Path = "/swagger";
+		config.DocumentPath = "/swagger/{documentName}/swagger.json";
+		config.DocExpansion = "list";
+	});
+}
 
 app.MapGet("/", () => "Hello World!");
 
